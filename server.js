cd4b4119d53aaf65df1ac0bc23af3f86e356186a
lokify <118039425+lokify@users.noreply.github.com>
2024-07-07 15:23:24 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,11 +1,13 @@
 const http = require('http');
 
+const PORT = process.env.PORT || 8080;
+
 const server = http.createServer((req, res) => {
   res.writeHead(200, { 'Content-Type': 'text/plain' });
   res.write('The bot is live');
   res.end();
 });
 
-server.listen(8080, () => {
-  console.log('Server is running on port 8080');
+server.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
 });
