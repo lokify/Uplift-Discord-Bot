c39065a0d82e257865302486c6038a24a8c1ee4b
lokify <118039425+lokify@users.noreply.github.com>
2024-07-07 16:33:10 +0530

    Update server.js

--- server.js
+++ server.js
@@ -7,8 +7,9 @@ const keepAlive = () => {
         res.end('The bot is alive!');
     });
 
-    server.listen(process.env.PORT || 10000, () => {
-        console.log(`Server is running on port ${process.env.PORT || 10000}`);
+    const PORT = process.env.PORT || 3000;
+    server.listen(PORT, () => {
+        console.log(`Server is running on port ${PORT}`);
     });
 };
 
