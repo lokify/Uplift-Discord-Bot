be7a93b3763d2db8ddc164f23837bcbeca79761f
lokify <118039425+lokify@users.noreply.github.com>
2024-07-07 14:36:15 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,11 +1,5 @@
-const express = require("express");
-const server=express();
-server.all("/", (req, res) =>{
-  res.send("Bot is running!")
-})
-function keepAlive(){
-  server.listen(3000,()=>{
-    console.log("Server is ready.")
-  })
-}
-module.exports=keepAlive
+var https=require('https');
+https.createServer(function(req,res){
+  res.write(" The bot is live ");
+  res.end();
+}).listen(8080);
